2ad13b7bbff6a898746666b8a8670d3eae0a6e2f
Denilsonxds <denilsonxds.dm@gmail.com>
2019-11-23 17:07:20 -0300

    adicionanado o update do cadastro

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -8,21 +8,27 @@ urlpatterns = [
     #-------funcionarios-----------
     url(r'^funcionarios/$', lista_funcionarios, name='cadastro_lista_funcionarios'), #url para cadastrar clientes
     url(r'^funcionarios_novo/$', funcionario_novo, name='cadastro_funcionario_novo'), #formulario de cadastro
+    url(r'^funcionarios_update(?P<id>\d+)/$', funcionario_update, name='cadastro_funcionario_update'),
     #---------clientes--------------
     url(r'^clientes/$', lista_clientes, name='cadastro_lista_clientes'), #url para cadastrar clientes
     url(r'^clientes_novo/$', cliente_novo, name='cadastro_cliente_novo'), #formulario cadastro
+    url(r'^clientes_update(?P<id>\d+)/$', cliente_update, name='cadastro_cliente_update'),
     #--------roupas---------
     url(r'^roupas/$', lista_roupas, name='cadastro_lista_roupas'), #cadastro de roupa
     url(r'^roupas_novo/$', roupa_novo, name='cadastro_roupa_novo'),
+    url(r'^roupas_update(?P<id>\d+)/$', roupa_update, name='cadastro_roupa_update'),
     #----------MarcaRoupa-------
     url(r'^marcaroupas/$', lista_marcaroupas, name='cadastro_lista_marcaroupas'),
     url(r'^marcaroupas_novo/$', marcaroupa_novo, name='cadastro_marcaroupa_novo'),
+    url(r'^marcaroupas_update(?P<id>\d+)/$', marcaroupa_update, name='cadastro_marcaroupa_update'),
     #---------TipoRoupa----------
     url(r'^tiporoupas/$', lista_tiporoupas, name='cadastro_lista_tiporoupas'),
     url(r'^tiporoupas_novo/$', tiporoupa_novo, name='cadastro_tiporoupa_novo'),
+    url(r'^tiporoupas_update(?P<id>\d+)/$', tiporoupa_update, name='cadastro_tiporoupa_update'),
     #----------Sessao---------
     url(r'^sessao/$', lista_sessao, name='cadastro_lista_sessao'),
     url(r'^sessao_novo/$', sessao_novo, name='cadastro_sessao_novo'),
+    url(r'^sessao_update(?P<id>\d+)/$', sessao_update, name='cadastro_sessao_update'),
 
     
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -23,6 +23,20 @@ def funcionario_novo(request):
         form.save()
     return redirect('cadastro_lista_funcionarios')
 
+def funcionario_update(request, id):
+    data = {}
+    funcionario = Funcionario.objects.get(id=id)
+    form = FuncionarioForm(request.POST or None, instance=funcionario)
+    data['funcionario'] = funcionario
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_lista_funcionarios')
+    else:
+        return render(request, 'cadastro/update_funcionario.html', data)
+
 #-------------Cliente----------------------
 
 
@@ -38,6 +52,20 @@ def lista_clientes(request):
     data = {'cliente' : cliente, 'form' : form}
     return render(request, 'cadastro/lista_clientes.html', data)
 
+def cliente_update(request, id):
+    data = {}
+    cliente = Cliente.objects.get(id=id)
+    form = ClienteForm(request.POST or None, instance=cliente)
+    data['cliente'] = cliente
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_lista_clientes')
+    else:
+        return render(request, 'cadastro/update_cliente.html', data)
+
 #----------Roupas --------------
 
 def lista_roupas(request):
@@ -52,6 +80,19 @@ def roupa_novo(request):
         form.save()
     return redirect('cadastro_lista_roupas')
 
+def roupa_update(request, id):
+    data = {}
+    roupa = Roupa.objects.get(id=id)
+    form = RoupaForm(request.POST or None, instance=roupa)
+    data['roupa'] = roupa
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_lista_roupas')
+    else:
+        return render(request, 'cadastro/update_roupa.html', data)
 
 #------------MarcaRoupas---------
 
@@ -67,6 +108,20 @@ def marcaroupa_novo(request):
         form.save()
     return redirect('cadastro_lista_marcaroupas')
 
+def marcaroupa_update(request, id):
+    data = {}
+    marcaroupa = MarcaRoupa.objects.get(id=id)
+    form = MarcaRoupaForm(request.POST or None, instance=marcaroupa)
+    data['marcaroupa'] = marcaroupa
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_lista_marcaroupas')
+    else:
+        return render(request, 'cadastro/update_marcaroupa.html', data)
+
 #-----------TipoRoupas----------
 
 def lista_tiporoupas(request):
@@ -81,6 +136,20 @@ def tiporoupa_novo(request):
         form.save()
     return redirect('cadastro_lista_tiporoupas')
 
+def tiporoupa_update(request, id):
+    data = {}
+    tiporoupa = TipoRoupa.objects.get(id=id)
+    form = TipoRoupaForm(request.POST or None, instance=tiporoupa)
+    data['tiporoupa'] = tiporoupa
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_lista_tiporoupas')
+    else:
+        return render(request, 'cadastro/update_tiporoupa.html', data)
+
 #--------Sessao--------------
 
 def lista_sessao(request):
@@ -94,3 +163,17 @@ def sessao_novo(request):
     if form.is_valid():
         form.save()
     return redirect('cadastro_lista_sessao')
+
+def sessao_update(request, id):
+    data = {}
+    sessao = Sessao.objects.get(id=id)
+    form = SessaoForm(request.POST or None, instance=sessao)
+    data['sessao'] = sessao
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro_lista_sessao')
+    else:
+        return render(request, 'cadastro/update_sessao.html', data)
